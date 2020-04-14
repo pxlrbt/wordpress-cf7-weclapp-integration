2217278da8e86bf1d29c638179ff391574219e22
Dennis Koch <info@pixelarbeit.de>
2020-04-14 08:04:52 +0200

    release: bump version

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WeClapp Integration for Contact Form 7
 Description: Send Contact Form 7 form data to WeClapp crm
-Version:     1.1
+Version:     1.2
 Author:      pixelarbeit
 Author URI:  https://pixelarbeit.de
 */
